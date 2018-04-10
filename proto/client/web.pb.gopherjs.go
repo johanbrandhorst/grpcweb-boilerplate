13183ba73ce95bc3e8b0f1606ef04908a627e26a
Johan Brandhorst <johan.brandhorst@gmail.com>
2018-04-10 22:10:12 +0100

    Upgrade protobuf dependency to v0.6.0

--- proto/client/web.pb.gopherjs.go
+++ proto/client/web.pb.gopherjs.go
@@ -31,7 +31,7 @@ var _ grpcweb.Client
 
 // This is a compile-time assertion to ensure that this generated file
 // is compatible with the grpcweb package it is being compiled against.
-const _ = grpcweb.GrpcWebPackageIsVersion2
+const _ = grpcweb.GrpcWebPackageIsVersion3
 
 // Client API for Backend service
 
